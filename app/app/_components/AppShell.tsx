0f220c054c4b4e17e23fd26a25dec9bf8a313088
Bogdan Gabriel Enica <bogdangabrielenica@MacBook-Air-Bogdan.local>
2025-09-14 14:17:47 +0300

    chore: updates

--- app/app/_components/AppShell.tsx
+++ app/app/_components/AppShell.tsx
@@ -21,10 +21,28 @@ export default function AppShell({ title, currentPath, children }: Props) {
           color: "var(--text)",
         }}
       >
-        <div style={{ maxWidth: 1200, margin: "0 auto", width: "100%" }}>
+        <div
+          style={{
+            maxWidth: 1200,
+            margin: "0 auto",
+            width: "100%",
+            paddingLeft: "calc(16px + var(--safe-left))",
+            paddingRight: "calc(16px + var(--safe-right))",
+          }}
+        >
           <AppHeader currentPath={currentPath} />
         </div>
-        <main style={{ padding: 16, maxWidth: 1200, margin: "0 auto", width: "100%" }}>
+        <main
+          style={{
+            maxWidth: 1200,
+            margin: "0 auto",
+            width: "100%",
+            paddingTop: 16,
+            paddingBottom: 16,
+            paddingLeft: "calc(16px + var(--safe-left))",
+            paddingRight: "calc(16px + var(--safe-right))",
+          }}
+        >
           {children}
         </main>
       </div>
