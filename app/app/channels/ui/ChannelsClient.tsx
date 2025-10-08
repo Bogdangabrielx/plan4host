93b69312506366648c6955b27fb84df99768141a
Bogdan Gabriel Enica <bogdangabrielenica@MacBook-Air-Bogdan.local>
2025-10-09 02:10:08 +0300

    chore: updates

--- app/app/channels/ui/ChannelsClient.tsx
+++ app/app/channels/ui/ChannelsClient.tsx
@@ -590,7 +590,7 @@ export default function ChannelsClient({ initialProperties }: { initialPropertie
               <button
                 className="sb-btn"
                 style={{
-                  width: isSmall ? '100%' : 180,
+                  width: isSmall ? '100%' : 300,
                   padding: '8px 12px',
                   borderRadius: isSmall ? 29 : 10,
                   border: '1px solid var(--border)',
@@ -616,7 +616,7 @@ export default function ChannelsClient({ initialProperties }: { initialPropertie
               <button
                 className="sb-btn"
                 style={{
-                  width: isSmall ? '100%' : 180,
+                  width: isSmall ? '100%' : 300,
                   padding: '8px 12px',
                   borderRadius: isSmall ? 29 : 10,
                   border: '1px solid var(--border)',
