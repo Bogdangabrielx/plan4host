dcbdfe99b9090dcbad11e17864c65f5fe9741097
Bogdan Gabriel Enica <bogdangabrielenica@MacBook-Air-Bogdan.local>
2025-10-24 11:44:54 +0300

    chore: updates

--- app/app/ui/BottomNav.tsx
+++ app/app/ui/BottomNav.tsx
@@ -143,7 +143,7 @@ useEffect(() => {
         left: 0,
         right: 0,
         bottom: 0,                 // ancorat de muchia fizică
-        background: "var(--panel)",
+        background: "transparent",
         borderTop: "1px solid var(--border)",
         borderTopLeftRadius: 23,
         borderTopRightRadius: 23,
