bf66b143f09452785e57c19cf64168f32b2784a7
Bogdan Gabriel Enica <bogdangabrielenica@MacBook-Air-Bogdan.local>
2025-10-28 00:21:02 +0200

    chore: updates

--- app/app/reservationMessage/ui/ReservationMessageClient.tsx
+++ app/app/reservationMessage/ui/ReservationMessageClient.tsx
@@ -1006,8 +1006,8 @@ export default function ReservationMessageClient({
           </div>
 
           <div style={{ display: "flex", gap: 8, marginTop: 10, flexWrap: "wrap" }}>
-            <button className="sb-cardglow" style={{ ...btn, borderRadius: 16 }} onClick={() => { const cur = composeBlocks(); setTpl(prev => ({ ...prev, ...(lang==='ro' ? { blocks: cur } : { blocks_en: cur }) })); saveDraft(); }} disabled={!isAdmin}>Save</button>
-            <button className="sb-cardglow" style={{ ...btnPri, borderRadius: 16 }}  onClick={() => { const cur = composeBlocks(); setTpl(prev => ({ ...prev, ...(lang==='ro' ? { blocks: cur } : { blocks_en: cur }) })); publish(); }} disabled={!isAdmin}>Make active</button>
+            <button className="sb-cardglow" style={{ ...btn, borderRadius: 16, padding: '8px 12px' }} onClick={() => { const cur = composeBlocks(); setTpl(prev => ({ ...prev, ...(lang==='ro' ? { blocks: cur } : { blocks_en: cur }) })); saveDraft(); }} disabled={!isAdmin}>Save</button>
+            <button className="sb-cardglow" style={{ ...btn, borderRadius: 16, padding: '8px 12px',borderColor:"var(--primary)" }}  onClick={() => { const cur = composeBlocks(); setTpl(prev => ({ ...prev, ...(lang==='ro' ? { blocks: cur } : { blocks_en: cur }) })); publish(); }} disabled={!isAdmin}>Make active</button>
           </div>
         </section>
       )}
