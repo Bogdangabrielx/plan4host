559629440d593c6f43e27d24afde9e342cccfb52
Bogdan Gabriel Enica <bogdangabrielenica@MacBook-Air-Bogdan.local>
2025-11-24 20:51:41 +0200

    chore: updates

--- app/app/guest/ui/GuestOverviewClient.tsx
+++ app/app/guest/ui/GuestOverviewClient.tsx
@@ -1652,7 +1652,7 @@ function EditFormBookingModal({
               <strong>Send info email to guest?</strong>
             </div>
             <div style={{ color:'var(--text)', marginBottom: 12 }}>
-              We will send an informational email including the assigned room number to the guest.<br/>
+              We will send an informational email including the assigned room to the guest.<br/>
               Do you want to send this email to the guest?
               {sendMailError && (
                 <div style={{ color:'var(--danger)', marginTop:8 }}>{sendMailError}</div>
