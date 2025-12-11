95f4fb5b7154d87fbcf963795387cacbbcf26928
Bogdan Gabriel Enica <bogdangabrielenica@MacBook-Air-Bogdan.local>
2025-12-11 09:39:40 +0200

    chore: updates

--- app/app/qr/ui/QrGeneratorClient.tsx
+++ app/app/qr/ui/QrGeneratorClient.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useMemo, useState } from "react";
-import QrWithLogo from "../../../QrWithLogo";
+import QrWithLogo from "@/components/QrWithLogo";
 
 const QR_SIZE = 320;
 
