74375a8cf9f78efa7e84a09b9f4bf548a99bb2db
Bogdan Gabriel <bogdangabriel@Bogdans-Mac-mini.local>
2026-04-03 16:43:00 +0300

    notifications

--- next.config.mjs
+++ next.config.mjs
@@ -5,8 +5,15 @@ const withPWAConfigured = withPWA({
   dest: "public",
   register: true,
   skipWaiting: true,
-  cacheOnFrontEndNav: true,
+  cacheStartUrl: false,
+  dynamicStartUrl: false,
+  cacheOnFrontEndNav: false,
   disable: process.env.NODE_ENV === "development" || process.env.NEXT_DISABLE_PWA === "1",
+  publicExcludes: [
+    "!noprecache/**/*",
+    "!**/*.{png,jpg,jpeg,JPG,JPEG,gif,webp,avif,mp4,MP4,mov,webm,psd,csv,pdf}",
+    "!fonts/**/*",
+  ],
   workboxOptions: {
     importScripts: ["push-sw.js"],
   },
